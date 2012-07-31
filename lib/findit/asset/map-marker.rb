1a2601bebbb0d828f2ec0283aea78247800a7247
Chip Rosenthal <chip@unicom.com>
2012-07-31 01:37:22 -0500

    Documentation comments.

--- lib/findit/asset/map-marker.rb
+++ lib/findit/asset/map-marker.rb
@@ -1,21 +1,35 @@
 require 'findit/asset/image'
 
 module FindIt
-  module Asset    
+  module Asset  
+    
+    #
+    # A MapMarker is a graphic image (and an optional shadow image to provide 3D effect) to
+    # identify a point on a map.
+    #
+    # A nice index of Google map markers can be viewed here:
+    # https://sites.google.com/site/gmapsdevelopment/
+    #
     class MapMarker
       
+      # Standard height (in pixels) of Google map icon images.
       DEFAULT_MARKER_HEIGHT = 32
+      
+      # Standard width (in pixels) of Google map icon images.
       DEFAULT_MARKER_WIDTH = 32
+      
+      # Standard width (in pixels) of Google map icon shadow images.
       DEFAULT_SHADOW_WIDTH = 59
       
+      # A FindIt::Asset::Image instance for the map icon graphic.
       attr_reader :marker
+      
+      # A FindIt::Asset::Image instance for the map icon shadow graphic,
+      # or nil if there is no shadow graphic for this marker.
       attr_reader :shadow
     
       #
       # Construct a new FindIt::Asset::MapMarker.
-      #
-      # A MapMarker is a graphic image (and an optional shadow image to provide 3D effect) to
-      # identify a point on a map.
       #
       # Parameters:
       #
@@ -29,9 +43,9 @@ module FindIt
       #
       # * :height => INTEGER -- Height of the image in pixels. (default: DEFAULT_MARKER_HEIGHT)
       # * :width => INTEGER -- Width of the image in pixels. (default: DEFAULT_MARKER_WIDTH)
-      # * :shadow => URL (default: none)
-      # * :height_shadow => INTEGER (default: same height as marker)
-      # * :width_shadow => INTEGER (default: DEFAULT_SHADOW_WIDTH)
+      # * :shadow => URL -- URL of the marker shadow image file. (default: none)
+      # * :height_shadow => INTEGER -- Height of the marker shadow image in pixels. (default: same height as marker)
+      # * :width_shadow => INTEGER -- Width of the marker shadow image in pixels. (default: DEFAULT_SHADOW_WIDTH)
       #
       # The :shadow URL can either be a full URL, or just a filename. If just a filename
       # is specified then the URL of the marker image is used as the base URL.
@@ -64,6 +78,24 @@ module FindIt
         end
       end
       
+      # Example:
+      #
+      #   m = FindIt::Asset::MapMarker.new(
+      #     "http://maps.google.com/mapfiles/kml/pal2/icon0.png",
+      #     :shadow => "icon0s.png")
+      #   m.to_h
+      #
+      # Produces:
+      #
+      #   {
+      #     :marker => {
+      #       :url => "http://maps.google.com/mapfiles/kml/pal2/icon0.png",
+      #       :height => 32, :width => 32},
+      #     :shadow => {
+      #       :url => "http://maps.google.com/mapfiles/kml/pal2/icon0s.png",
+      #       :height => 32, :width => 59}
+      #   }
+      #
       def to_h
         h = {}
         h[:marker] = @marker.to_h
