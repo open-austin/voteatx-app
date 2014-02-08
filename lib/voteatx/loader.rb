e4654797ad41007b3753e2d8ea16dfcf9e22a0b6
Chip Rosenthal <chip@unicom.com>
2014-02-08 15:12:38 -0600

    lib/voteatx/loader.rb:
    * Refactored way import data is acquired, adding a
      VotingPlacesLoader#col_id_to_index property and a CSV_Row#field_by_id
      method.
    
    * Allow indexes in EARLY_VOTING_FIXED_HOURS[] to be matched by
      "#{code}|#{sitename}" as well as just "#{code}". Before we had to
      go into the spreadsheet and edit the code "V" locations with a code
      specific for that location (say "V1"). Now, we don't have to edit the
      spreadsheet, and just use an index such as "V|Dan Ruiz Public Library".
    
    * The DEFAULT_COL_IDS values are now all arrays.
    
    * Renamed VotingPlacesLoader#col_name property to
    VotingPlacesLoader#col_id_to_names.
    
    * New private methods init_col_id_to_idx_table() and add_row_methods().
    
    * The cleanup_row() method now invokes add_row_methods(), to monkeypatch
      field_by_id() onto the row and initialize the index table.
    
    * Dropped ensure_not_empty() method, because new field_by_id() method
      checks for empty values (by default).
    
    * Fixed bug in get_date() where it was failing to parse single-digit
      months.
    
    * Improved warning displayed by make_location() when it encounters
      inconsistent data.
    
    * Changed load_evfixed_places(), to locate EARLY_VOTING_FIXED_HOURS[]
      records by either "#{code}" or "#{code}|#{sitename}".

--- lib/voteatx/loader.rb
+++ lib/voteatx/loader.rb
@@ -2,6 +2,7 @@ require 'findit-support'
 require 'logger'
 require 'csv'
 require 'yaml'
+require 'pp' # for debug
 
 class NilClass
   def empty?
@@ -101,20 +102,23 @@ module VoteATX
   #
   class VotingPlacesLoader
 
-    # Default mapping of data columns to spreadsheet column names
-    DEFAULT_COL_NAMES = {
-      :SITE_NAME => "Name",
-      :PCT => "Pct",
-      :COMBINED_PCTS => "Combined Pcts.",
-      :LOCATION_ADDRESS => "Address",
-      :LOCATION_CITY => "City",
-      :LOCATION_ZIP => "Zipcode",
-      :LOCATION_LONGITUDE => "Longitude",
-      :LOCATION_LATITUDE => "Latitude",
-      :SCHEDULE_CODE => "Hours",
-      :SCHEDULE_DATE => "Date",
-      :SCHEDULE_TIME_OPENS => "Start Time",
-      :SCHEDULE_TIME_CLOSES => "End Time",
+    # Default mapping of identifiers to spreadsheet column names
+    #
+    # Used to initialize @col_id_to_names when constructing a new loader.
+    #
+    DEFAULT_COL_IDS = {
+      :SITE_NAME => ["Name", "Site Name"],
+      :PCT => ["Pct"],
+      :COMBINED_PCTS => ["Combined Pcts."],
+      :LOCATION_ADDRESS => ["Address", "Site Address"],
+      :LOCATION_CITY => ["City"],
+      :LOCATION_ZIP => ["Zipcode"],
+      :LOCATION_LONGITUDE => ["Longitude"],
+      :LOCATION_LATITUDE => ["Latitude"],
+      :SCHEDULE_CODE => ["Hours"],
+      :SCHEDULE_DATE => ["Date"],
+      :SCHEDULE_TIME_OPENS => ["Start Time"],
+      :SCHEDULE_TIME_CLOSES => ["End Time"],
     }
 
     # Name of the database we are creating
@@ -129,8 +133,32 @@ module VoteATX
     # Spatialite database instance
     attr_reader :db
 
-    # Mapping of data columns to spreadsheet column names
-    attr_accessor :col_name
+    # Mapping of data columns identifiers to spreadsheet column names.
+    #
+    # The column names have been known to change in different
+    # data dumps -- sometimes even among sheets in a single dump.
+    #
+    # This table maps site ids, such as :SITE_NAME, to a list
+    # of names that may appear in the spreadsheet as a title
+    # for the corresponding column.
+    #
+    # When a new instance is constructed, this is initialized to
+    # DEFAULT_COL_IDS. If you encounter changes to the spreadsheets,
+    # best practice would be to update the DEFAULT_COL_IDS value.
+    # (Unless the change is a one-off situation, in which case you might
+    # want to just patch the table created in the instance.)
+    #
+    attr_accessor :col_id_to_names
+
+    # Mapping of data column identifers to spreadsheet column index values.
+    #
+    # See the "@col_id_to_names" discussion.
+    #
+    # This table maps the index to column index number for the spreadsheet
+    # currently being processed. This is managed automatically by the
+    # FIXME method.
+    #
+    attr_reader :col_id_to_index
 
     # Numeric range for valid longitude (degrees) values
     attr_accessor :valid_lng_range
@@ -182,7 +210,8 @@ module VoteATX
       @db.logger = @log
       @db.sql_log_level = :debug
 
-      @col_name = DEFAULT_COL_NAMES.dup
+      @col_id_to_names = DEFAULT_COL_IDS.dup
+      @col_id_to_index = {}
       @valid_lng_range = -180 .. 180
       @valid_lat_range = -180 .. 180
       @valid_zip_regexp = /^\d\d\d\d\d(-\d\d\d\d)?$/
@@ -190,42 +219,108 @@ module VoteATX
       @log.info("loading database \"#{@dbname}\" ...")
     end
 
+
+    # Setup the @col_id_to_idx table to map column identifiers (such
+    # as :PCT) to their index into the row.
+    #
+    # This should be called each time a CSV row is processed. (Will
+    # do nothing if a current mapping already is defined.)
+    #
+    # Typically this is done automatically by add_row_methods().
+    #
+    def init_col_id_to_idx_table(row)
+
+      # Mapping is up-to-date.
+      return if ! @col_id_to_idx.empty? && @col_id_to_idx[:__ROWS] == row.headers()
+
+      @col_id_to_idx = {:__ROWS => row.headers()}
+
+      @col_id_to_names.each do |id, names|
+	names.each do |name|
+	  if row.has_key?(name)
+	    @col_id_to_idx[id] = row.index(name)
+	    break
+	  end
+	end
+      end
+#pp({'@col_id_to_idx' => @col_id_to_idx})
+
+    end
+    private :init_col_id_to_idx_table
+
+
+    # Add the field_by_id() method to this row.
+    #
+    # Also initializes the @col_id_to_idx table, with column mapping
+    # information for the CSV currently being processed.
+    #
+    # Typically, this is done automatically by cleanup_row().
+    #
+    def add_row_methods(row)
+
+      init_col_id_to_idx_table(row)
+
+      class << row
+        attr_accessor :col_id_to_idx
+
+	# Get a cell value from the spreadsheet row, identifed by column id
+	# The column id maps to a column name -- or group of possible names.
+        def field_by_id(id, args = {})
+	  empty_ok = args.has_key?(:empty_ok) ? args[:empty_ok] : false
+	  idx = @col_id_to_idx[id]
+	  raise "Invalid column id \"#{id}\" for row: #{row}" unless idx
+	  value = self.field(idx)
+	  if value.empty? && ! empty_ok
+	    raise "Required field \"#{id}\" (index #{idx}) empty for row: #{row}"
+	  end
+#pp({'id' => id, 'idx' => idx, 'value' => value})
+	  return value
+	end
+      end
+
+      row.col_id_to_idx = @col_id_to_idx
+    end
+    private :add_row_methods
+
+
     # Cleanup a row of data read from the spreadsheet.
+    #
+    # Also calls (private method) add_row_methods() to setup required
+    # methods and information for this row.  Thus, it's important to
+    # call this method on a row before doing anything else
+    #
     def cleanup_row(row)
+
+      # Add field_by_id() support for this row.
+      add_row_methods(row)
+
+      # Execute a value-specific cleanup on each column.
       row.each {|k,v| row[k] = v.cleanup}
 
       # Convert "Combined @ 109 Parmer Lane Elementary School" -> "Parmer Lane Elementary School"
-      row[@col_name[:SITE_NAME]].sub!(/^Combined\s+@\s+\d+\s+/, "")
-    end
+      row.field_by_id(:SITE_NAME).sub!(/^Combined\s+@\s+\d+\s+/, "")
 
-    # Assert that the spreadsheet row has non-empty values for each of the specifieid columns.
-    def ensure_not_empty(row, *cols)
-      cols.each do |col|
-        raise "required column \"#{col}\" not defined: #{row}" if row[col].empty?
-      end
     end
 
     # Extract date as [mm,dd,yyyy] from specified col, in form "MM/DD/YYYY"
-    def get_date(row, col)
-      ensure_not_empty(row, col)
-      m = row[col].match(%r[^(\d\d)/(\d{1,2})/(\d\d\d\d)$])
-      raise "bad #{col} value \"#{row[col]}\": #{row}" unless m && m.length-1 == 3
+    def get_date(row, id)
+      m = row.field_by_id(id).match(%r[^(\d{1,2})/(\d{1,2})/(\d\d\d\d)$])
+      raise "bad #{id} value \"#{row.field_by_id(id)}\": #{row}" unless m && m.length-1 == 3
       m.captures.map {|s| s.to_i}
     end
 
     # Extract time as [hh,mm] from specified col, in form "HH:MM"
-    def get_time(row, col)
-      ensure_not_empty(row, col)
-      m = row[col].match(%r[^(\d{1,2}):(\d\d)$])
-      raise "bad #{col} value \"#{row[col]}\": #{row}" unless m && m.length-1 == 2
+    def get_time(row, id)
+      m = row.field_by_id(id).match(%r[^(\d{1,2}):(\d\d)$])
+      raise "bad #{id} value \"#{row.field_by_id(id)}\": #{row}" unless m && m.length-1 == 2
       m.captures.map {|s| s.to_i}
     end
 
     # Produce (start_time .. end_time) range from info in database record
     def get_datetimes(row)
-      mm, dd, yyyy = get_date(row, @col_name[:SCHEDULE_DATE])
-      start_hh, start_mm = get_time(row, @col_name[:SCHEDULE_TIME_OPENS])
-      end_hh, end_mm = get_time(row, @col_name[:SCHEDULE_TIME_CLOSES])
+      mm, dd, yyyy = get_date(row, :SCHEDULE_DATE)
+      start_hh, start_mm = get_time(row, :SCHEDULE_TIME_OPENS)
+      end_hh, end_mm = get_time(row, :SCHEDULE_TIME_CLOSES)
       Time.local(yyyy, mm, dd, start_hh, start_mm) .. Time.local(yyyy, mm, dd, end_hh, end_mm)
     end
 
@@ -341,34 +436,31 @@ module VoteATX
     end
 
 
-    # Create an entry in the "voting_locations" table for this location, return row id.
+    # Create an entry in the "voting_locations" table for this location,
+    # return database row id.
     #
-    # If the location already exists in the database, will return row id for existing row.
+    # If the location already exists in the database, will return database
+    # row id for existing row.
     #
-    # The "values" list must define: "Name", "Address", "City", "Zipcode", "Longitude", "Latitude".
+    # The "row" parameter is a CSV row that must define: :SITE_NAME,
+    # :LOCATION_ADDRESS, :LOCATION_CITY, :LOCATION_ZIP, :LOCATION_LONGITUDE,
+    # :LOCATION_LATITUDE.
     #
-    def make_location(values)
-      ensure_not_empty(values,
-        @col_name[:SITE_NAME],
-        @col_name[:LOCATION_ADDRESS],
-        @col_name[:LOCATION_CITY],
-        @col_name[:LOCATION_ZIP],
-        @col_name[:LOCATION_LONGITUDE],
-        @col_name[:LOCATION_LATITUDE])
+    def make_location(row)
 
-      lng = values[@col_name[:LOCATION_LONGITUDE]].to_f
-      raise "longitude \"#{lng}\" outside of expected range (#{@valid_lng_range}): #{values}" unless @valid_lng_range.include?(lng)
+      lng = row.field_by_id(:LOCATION_LONGITUDE).to_f
+      raise "longitude \"#{lng}\" outside of expected range (#{@valid_lng_range}): #{row}" unless @valid_lng_range.include?(lng)
 
-      lat = values[@col_name[:LOCATION_LATITUDE]].to_f
-      raise "latitude \"#{lat}\" outside of expected range (#{@valid_lat_range}): #{values}" unless @valid_lat_range.include?(lat)
+      lat = row.field_by_id(:LOCATION_LATITUDE).to_f
+      raise "latitude \"#{lat}\" outside of expected range (#{@valid_lat_range}): #{row}" unless @valid_lat_range.include?(lat)
 
-      zip = values[@col_name[:LOCATION_ZIP]]
+      zip = row.field_by_id(:LOCATION_ZIP)
       raise "bad zip value \"Zipcode\": #{zip}" unless zip =~ @valid_zip_regexp
 
       rec = {
-        :name => values[@col_name[:SITE_NAME]],
-        :street => values[@col_name[:LOCATION_ADDRESS]],
-        :city => values[@col_name[:LOCATION_CITY]],
+        :name => row.field_by_id(:SITE_NAME),
+        :street => row.field_by_id(:LOCATION_ADDRESS),
+        :city => row.field_by_id(:LOCATION_CITY),
         :state => "TX",
         :zip => zip,
         :geometry => Sequel.function(:MakePoint, lng, lat, 4326),
@@ -378,17 +470,21 @@ module VoteATX
         + rec[:street] + "\n" \
         + rec[:city] + ", " + rec[:state] + " " + rec[:zip]
 
-      loc = @db[:voting_locations] \
+      rec_stored = @db[:voting_locations] \
         .filter{ST_Equals(:geometry, MakePoint(lng, lat, 4326))} \
         .first
 
-      if loc
+      if rec_stored
         [:name, :street, :city, :state, :zip].each do |field|
-          if loc[field] != rec[field]
-            @log.warn("make_location: voting_locations(id #{loc[:id]}): inconsistent \"#{field}\" values [\"#{loc[field]}\", \"#{rec[field]}\"]")
+          if rec_stored[field] != rec[field]
+            @log.warn("make_location: \"#{field}\" value inconsistent with value in \"locations\" table");
+            @log.warn("  record id = \"#{rec_stored[:id]}\"")
+            @log.warn("  location name = \"#{rec_stored[:name]}\"") unless field == :name
+            @log.warn("  stored #{field} = \"#{rec_stored[field]}\"")
+            @log.warn("  new #{field}    = \"#{rec[field]}\"")
           end
         end
-        return loc
+        return rec_stored
       end
 
       id = @db[:voting_locations].insert(rec)
@@ -431,15 +527,14 @@ module VoteATX
 
         cleanup_row(row)
 
-        ensure_not_empty(row, @col_name[:PCT])
-        precinct = row[@col_name[:PCT]].to_i
+        precinct = row.field_by_id(:PCT).to_i
         raise "failed to parse precinct from: #{row}" if precinct == 0
 
         location = make_location(row)
 
         notes = nil
-        unless row[@col_name[:COMBINED_PCTS]].empty?
-          a = [precinct] + row[@col_name[:COMBINED_PCTS]].split(",").map {|s| s.to_i}
+        unless row.field_by_id(:COMBINED_PCTS, :empty_ok => true).empty?
+          a = [precinct] + row.field_by_id(:COMBINED_PCTS).split(",").map {|s| s.to_i}
           notes = "Combined precincts " + a.sort.join(", ")
         end
 
@@ -469,11 +564,21 @@ module VoteATX
 
         cleanup_row(row)
 
+        # There are a number of rows with site names such as "Mobile 1"
+        # with no data.
+        next if row.field_by_id(:SITE_NAME) =~ /^Mobile/ && row.field_by_id(:SCHEDULE_CODE, :empty_ok => true).empty?
+
         location = make_location(row)
 
-        schedule_code = row[@col_name[:SCHEDULE_CODE]]
-        schedule = schedule_by_code[schedule_code]
-        raise "unknown schedule code \"#{schedule_code}\": #{row}" unless schedule
+        sc1 = row.field_by_id(:SCHEDULE_CODE)
+        sc2 = sc1 + "|" + row.field_by_id(:SITE_NAME)
+        schedule = if schedule_by_code.has_key?(sc1)
+          schedule_by_code[sc1]
+        elsif schedule_by_code.has_key?(sc2)
+          schedule_by_code[sc2]
+        else
+          raise "unknown schedule code \"#{sc1}\" (also tried \"#{sc2}\"): #{row}" unless schedule
+        end
 
         @db[:voting_places] << {
           :place_type => "EARLY_FIXED",
